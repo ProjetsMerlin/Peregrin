9c5d506eb939d94222d623fc5f5d6f2f9fe9eff3
ProjetsMerlin <infolintermediaire@gmail.com>
2024-10-24 21:32:33 +0200

    maj

--- data/lang/fr.php
+++ data/lang/fr.php
@@ -1,7 +1,12 @@
 <?php
-
+/* HEAD BALISES*/
 $trad["lang_code"] = "fr-BE";
+$trad['site_description'] = "Peregrin Touque !";
 
-$trad["welcome"] = "bienvenue";
+/* title page */
+$trad["title_home"] = "Bienvenue";
+$trad["title_blog"] = "Blog fr";
 
-$trad["blog"] = "blog fr";
+/* rewiting */
+$trad["url_home"] = "accueil";
+$trad["url_blog"] = "blog";
--- data/rewriting.php
+++ data/rewriting.php
@@ -1,24 +1,15 @@
 <?php
-$trad["fr_blog"] = "blog";
-$trad["nl_blog"] = "actualiteit";
 
 switch (true) {
-case ($get_page === $trad["fr_home"] && $get_lang === "fr" ) :
-$slug_page = "home";
-break;
-
-case ($get_page === $trad["nl_home"] && $get_lang === "nl" ) :
+case ($get_page === $trad["url_home"] ) :
 $slug_page = "home";
 break;
 
 /* START */
-case ($get_page === $trad["fr_blog"] && $get_lang === "fr" ) :
+case ($get_page === $trad["url_blog"] ) :
 $slug_page = "blog";
 break;
 
-case ($get_page === $trad["nl_blog"] && $get_lang === "nl" ) :
-$slug_page = "blog";
-break;
 /* -END- */
 
 case ($get_page === "error" || $get_page === "404"):
