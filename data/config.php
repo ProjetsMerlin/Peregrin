753f35c914eb6a7bfcb4497429a1e120cb64c714
ProjetsMerlin <infolintermediaire@gmail.com>
2024-11-22 14:19:13 +0100

    v 1.2

--- data/config.php
+++ data/config.php
@@ -1,9 +1,9 @@
 <?php
 /* PARAMETERS */
-$url_local = 'localhost/peregrin';
+$url_local = 'localhost/projets/peregrin';
 $url_online = 'monsite.be';
 $possible_langage = array('fr', 'nl');
-$site_version = 1.0;
+$site_version = 1.2;
 
 $trad['fr_home'] = "accueil";
 $trad['nl_home'] = "welkom";
@@ -21,4 +21,14 @@ if($_SERVER['SERVER_NAME'] === 'localhost') {
 else {
   $gmap_key = 'xxx';
   $gcaptcja_siteKey = 'xxx';
+}
+
+function peregrin_ucfirst($input) {
+  $lowercaseString = strtolower($input);
+  $transformedString = ucfirst($lowercaseString);
+  return $transformedString;
+}
+
+function peregrin_authBasic($username,$password) {
+  return 'Authorization: Basic ' . base64_encode($username . ':' . $password);
 }
